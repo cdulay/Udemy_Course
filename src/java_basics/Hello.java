db8ca31b789964c9ca37eb364fb66d05a4f42778
cwdulay <cwdulay@gmail.com>
2018-02-05 12:37:06 -0500

    Initial Commit

--- src/java_basics/Hello.java
+++ src/java_basics/Hello.java
@@ -3,7 +3,7 @@ package java_basics;
 public class Hello
 {
 
-    public static void main(String [] args)
+    public static void main(String[] args)
     {
         System.out.println("Hello World!!!");
     }
